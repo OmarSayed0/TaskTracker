23164a067770ef696a54937cbecbe7d6a186caa1
Omar Sayed <151388740+OmarSayed0@users.noreply.github.com>
2025-02-05 00:04:12 +0200

    implement Application Logic

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -6,21 +6,104 @@ Features :
 3. Update Task
 4. View All Task
 5. Exit
- 
-*/
+ */
+
 namespace TaskTracker
 {
     public class Program
     {
+        static string[] Tasks = new string[100];
+        static int TaskIndex = 0;
         static void Main(string[] args)
         {
+
             Console.WriteLine("""
                 ---------------------------------
                 Welcome To my simple Task Tracker
                 ---------------------------------
                 """);
 
+            while (true)
+            {
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Choose an option : ");
+
+                Console.WriteLine("1. Add Task");
+                Console.WriteLine("2. Delete Task");
+                Console.WriteLine("3. Update Task");
+                Console.WriteLine("4. View All Task");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("--------------------");
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            AddTask();
+                            break;
+                        case 2:
+                            DeleteTask();
+                            break;
+                        case 3:
+                            UpdateTask();
+                            break;
+                        case 4:
+                            ViewAllTask();
+                            break;
+                        case 5:
+                            Console.WriteLine("Thank You for using Task Tracker");
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+
 
         }
+        private static void AddTask()
+        {
+            Console.WriteLine("Enter Task Name : ");
+            string taskName = Console.ReadLine();
+            Tasks[TaskIndex] = taskName;
+            TaskIndex++;
+            Console.WriteLine("Task Added Successfully");
+        }
+        private static void DeleteTask()
+        {
+            Console.WriteLine("Enter Task Number : ");
+            string taskNum = Console.ReadLine();
+            int taskID = Convert.ToInt32(taskNum);
+            Tasks[taskID-1] = "";
+            Console.WriteLine("Task Deleted Successfully");
+        }
+        private static void UpdateTask()
+        {
+            Console.WriteLine("Enter Task Number : ");
+            string taskNum = Console.ReadLine();
+            int taskID = Convert.ToInt32(taskNum);
+            Console.WriteLine("Enter New Task Name : ");
+            string newTaskName = Console.ReadLine();
+            Tasks[taskID-1] = newTaskName;
+            Console.WriteLine("Task Updated Successfully");
+        }
+        private static void ViewAllTask()
+        {
+            for (int i = 0; i < TaskIndex+1; i++)
+            {
+                if (Tasks[i] != null)
+                {
+                    Console.WriteLine($"{i+1}. {Tasks[i]}");
+                }
+            }
+            Console.WriteLine("                   - All Tasks You have");
+        }
     }
+
 }
