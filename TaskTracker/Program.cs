bcf165e2d1a15483edbda44a54852be884b80c52
Omar Sayed <151388740+OmarSayed0@users.noreply.github.com>
2025-02-04 23:07:46 +0200

    Intial the project and the description

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace TaskTracker
+﻿//Task Tracker App 
+/*
+Features :
+1. Add Task
+2. Delete Task
+3. Update Task
+4. View All Task
+5. Exit
+ 
+*/
+namespace TaskTracker
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("""
+                ---------------------------------
+                Welcome To my simple Task Tracker
+                ---------------------------------
+                """);
+
+
         }
     }
 }
